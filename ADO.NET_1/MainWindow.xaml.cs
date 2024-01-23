af00269af91182e0a17c9b5f9f2d7d9edc512ed0
Huseyn <horucov148@gmail.com>
2024-01-23 22:44:09 +0400

    Add project files.

---
+++ ADO.NET_1/MainWindow.xaml.cs
@@ -0,0 +1,101 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Text;
+using System.Windows;
+using System.Windows.Documents;
+using Microsoft.Data.SqlClient;
+
+namespace ADO.NET_1
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public class Authorr
+        {
+            public int Id { get; set; }
+            public string FirstName { get; set; }
+            public string LastName { get; set; }
+
+            public override string ToString()
+            {
+                
+                return $"{Id}  {FirstName}  {LastName}";
+            }
+        }
+
+        public class Categoryy
+        {
+            public int Id { get; set; }
+            public string Name { get; set; }
+
+            public override string ToString()
+            {
+
+                return $"{Id}  {Name}  ";
+            }
+        }
+
+        public ObservableCollection<Authorr> Authorrs { get; set; }
+        public ObservableCollection<Categoryy> Categoryys { get; set; }
+        public MainWindow()
+        {
+            InitializeComponent();
+            Authorrs = new ObservableCollection<Authorr>();
+            Categoryys = new ObservableCollection<Categoryy>();
+            LoadData();
+            DataContext = this;
+        }
+
+        private void LoadData()
+        {
+            SqlConnection connection = new SqlConnection("Data Source=DESKTOP-SDREM5J\\SQLEXPRESS;Initial Catalog=Library;Integrated Security=True;Connect Timeout=30;Encrypt=False;Trust Server Certificate=False;Application Intent=ReadWrite;Multi Subnet Failover=False;Trust Server Certificate=True;");
+            try
+            {
+                connection.Open();
+                string query = "SELECT * FROM Authors";
+                SqlCommand command = new SqlCommand(query, connection);
+                SqlDataReader reader = command.ExecuteReader();
+
+                while (reader.Read())
+                {
+                    Authorrs.Add(new Authorr { Id = reader.GetInt32(0), FirstName = reader.GetString(1), LastName = reader.GetString(2) });
+                }
+            }
+            catch(Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+            finally
+            { connection.Close(); }
+
+
+            try
+            {
+                connection.Open();
+                string query_category = "SELECT * FROM Categories";
+                SqlCommand command1 = new SqlCommand(query_category, connection);
+                SqlDataReader reader1 = command1.ExecuteReader();
+
+                while (reader1.Read())
+                {                  
+                    Categoryys.Add(new Categoryy { Id = reader1.GetInt32(0), Name = reader1.GetString(1) });
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+            finally
+            { connection.Close(); }
+
+        }
+
+        private void aut_SelectionChanged(object sender, System.Windows.Controls.SelectionChangedEventArgs e)
+        {
+            cat.IsEnabled = true;
+        }
+    }
+}
---
+++ ADO.NET_2/MainWindow.xaml.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using Microsoft.Data.SqlClient;
+
+namespace ADO.NET_2
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void btn_Click(object sender, RoutedEventArgs e)
+        {
+            SqlConnection connection = new SqlConnection("Data Source=DESKTOP-SDREM5J\\SQLEXPRESS;Initial Catalog=Library;Integrated Security=True;Connect Timeout=30;Encrypt=False;Trust Server Certificate=False;Application Intent=ReadWrite;Multi Subnet Failover=False;Trust Server Certificate=True;");
+
+            try
+            {
+                connection.Open();
+                string order = Order.Text;
+                SqlCommand command = new SqlCommand(order, connection);
+                command.ExecuteNonQuery();
+
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.ToString());
+            }
+            finally
+            { connection.Close(); }
+        }
+
+
+    }
+}
